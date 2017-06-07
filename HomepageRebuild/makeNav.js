6375dbea3c6d6fab55e31da22ab00c5ab5693da9
Tim Plumb <plumbt@toa.st>
2017-06-07 17:31:09 +0100

    Rebuild

---
+++ HomepageRebuild/makeNav.js
@@ -0,0 +1,49 @@
+// function createDropDowns(){
+
+
+// }
+
+// function showDropDown() {
+	
+// 	$(".container").prepend("<div class='dropdownBox' style='width:"+$(".container").width()+"px'></div")
+// 	$( ".dropdownBox" ).hover(
+//   		function() {
+// 	  		onHoverBox = true
+// 	  		console.log("out")
+//   		}, function() {
+//   	 		onHoverBox = false
+//   	 		hideDropDown() 
+//   	 		console.log(onHoverBox)
+//   		}
+// 	)
+// }
+
+// function hideDropDown() {
+// 	$(".dropdownBox").remove()
+// }
+
+// $( document ).ready(function() {
+    
+// });
+
+
+// $( ".dropdownBox" ).hover(
+//   function() {
+// 	  onHoverBox = true;
+//   }, function() {
+//   	 onHoverBox = false
+//   }
+// );
+
+// $( ".mainNav li" ).hover(
+
+//   function() {
+//   	console.log(this.id)
+//     showDropDown()
+//   }, function() {
+//   	if (!onHoverBox || onHoverBox != null){
+//   		console.log(onHoverBox)
+//     	hideDropDown()
+// 	}
+//   }
+// );
